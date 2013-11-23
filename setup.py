835570ba7b02e91a42d6d53dd391ea90cf887d5b
Steven Allen <steven@stebalien.com>
2013-11-23 13:39:44 -0500

    Remove '-extra-' from package name.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "overkill-extra-pulseaudio",
+    name = "overkill-pulseaudio",
     version = "0.1",
     install_requires=["overkill"],
     packages = find_packages(),
